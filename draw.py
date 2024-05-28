266d3d80df152e9c6a13ed3e3048b831eca954d7
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2024-05-28 11:35:13 -0400

    Append Output file with next Stroke. Continuous Drawing

--- draw.py
+++ draw.py
@@ -2,25 +2,43 @@ import tkinter as tk
 import numpy as np
 from PIL import Image, ImageDraw
 
+winHight = 100
+winWidth = 100
+
 class DrawingApp:
     def __init__(self, master):
         self.master = master
-        self.canvas = tk.Canvas(self.master, width=500, height=500, bg='white')
+        self.canvas = tk.Canvas(self.master, width = winWidth, height = winHight, bg = 'white')
         self.canvas.pack()
-        self.image = Image.new('RGB', (500, 500), 'white')
+        self.image = Image.new('RGB', (winWidth, winHight), 'white')
         self.draw = ImageDraw.Draw(self.image)
+        self.last_x = None
+        self.last_y = None
         self.canvas.bind("<B1-Motion>", self.draw_line)
         self.canvas.bind("<ButtonRelease-1>", self.save_image)
 
     def draw_line(self, event):
-        self.canvas.create_line(event.x, event.y, event.x + 1, event.y + 1, fill='black')
-        self.draw.line([event.x, event.y, event.x + 1, event.y + 1], fill='black')
+        if self.last_x is not None and self.last_y is not None:
+            self.canvas.create_line(self.last_x, self.last_y, event.x, event.y, fill='black')
+            self.draw.line([self.last_x, self.last_y, event.x, event.y], fill='black')
+        self.last_x = event.x
+        self.last_y = event.y
 
     def save_image(self, event):
+        self.last_x = None
+        self.last_y = None
         self.image.save('canvas.png')
         img_array = np.array(self.image.convert('L'))
-        np.savetxt("myfile.txt", img_array)
-        print(img_array)
+        #img_array = np.where(img_array < 128, 0, 1) converts to binary values
+        img_array = img_array / 255.0
+        np.savetxt("tempImage.txt", img_array)
+        append_file("tempImage.txt", "ouput.txt")
+
+
+def append_file(file1_path, file2_path):
+    with open(file1_path, 'r') as file1:
+        with open(file2_path, 'a+') as file2:
+            file2.write(file1.read())   
 
 root = tk.Tk()
 app = DrawingApp(root)
---
+++ testing.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+testArray = np.array([1,2,3,4,5,6,7])
+testList = list(testArray)
+for x in testList:
+    print(x)
