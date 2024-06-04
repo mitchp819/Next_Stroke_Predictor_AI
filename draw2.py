3cf2fcb453c5f9ed61484ebbd64e98415b3a4ac1
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2024-06-04 11:23:31 -0400

    Drawing Produce PNG
    A successful iteration where the canvas and the stroke is exported as a png

--- draw2.py
+++ draw2.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageDraw
 
 winHight = 500
 winWidth = winHight
-imageFmt = '%.3f'
 
 class DrawingApp:
     def __init__(self, master):
@@ -12,7 +11,7 @@ class DrawingApp:
         self.canvas = tk.Canvas(self.master, width = winWidth, height = winHight, bg = 'white')
         self.canvas.pack()
         self.stroke_canvas = tk.Canvas(self.master, width = winWidth, height = winHight, bg = 'white')
-        self.stroke_canvas.pack()
+        #self.stroke_canvas.pack()
         self.image = Image.new('RGB', (winWidth, winHight), 'white')
         self.draw = ImageDraw.Draw(self.image)
         self.last_x = None
@@ -21,9 +20,10 @@ class DrawingApp:
         self.canvas.bind("<B1-Motion>", self.draw_line)
         self.canvas.bind("<Button-1>", self.new_image)
         self.canvas.bind("<ButtonRelease-1>", self.save_image)
-    
+        self.image.save(f'canvas_{self.stroke_count}.png')
+
     def new_image(self, event):
-        self.stroke_image = Image.new('RGB', (winWidth, winHight), 'white')
+        self.stroke_image = Image.new('RGBA', (winWidth, winHight), (0,0,0,0))
         self.stroke_draw = ImageDraw.Draw(self.stroke_image)
 
     def draw_line(self, event):
@@ -39,7 +39,7 @@ class DrawingApp:
         self.last_x = None
         self.last_y = None
         self.stroke_image.save(f'stroke_{self.stroke_count}.png')
-        self.image.save(f'canvas_{self.stroke_count}.png')
+        self.image.save(f'canvas_{self.stroke_count + 1}.png')
         self.stroke_count += 1
  
 
