4eab8aaad209c5d219f92eb27b753e8bd013e4f8
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2024-06-05 10:35:50 -0400

    save all data as pickled list

---
+++ pickle_data.py
@@ -0,0 +1,14 @@
+import numpy as np
+import glob
+import os
+import pickle
+
+path = os.path.join(os.path.dirname(__file__), 'ImageData/*.npy')
+files = sorted(glob.glob(path))
+arrays = []
+
+for f in files:
+    arrays.append(np.load(f))
+
+with open("AllImageData.pkl", "wb") as file:
+    pickle.dump(arrays, file)
