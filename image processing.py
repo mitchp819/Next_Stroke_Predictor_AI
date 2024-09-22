5ae9bbe250c43c740db8f5ec5bbab6b0b922dcad
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2024-09-22 12:23:18 -0400

    Whoah a Spirtual Machine is born
    Oy this is producing interesting content

--- image processing.py
+++ image processing.py
@@ -1,11 +1,13 @@
 import numpy as np
 from PIL import Image
-
+import random
 
 
 class img_processer:
     def __init__(self, data_set_128_file: str, data_set_64_file: str = None, data_set_32_file: str = None, data_set_16_file: str = None, data_set_8_file: str= None, data_set_4_file: str = None, input_data = None):
         self.data_set = np.load(data_set_128_file)
+        np.random.shuffle(self.data_set)
+        
         print(f"Data Set of shape {self.data_set.shape} Loaded")
 
         if data_set_64_file != None:
@@ -94,32 +96,91 @@ class img_processer:
         downscaled16 = img_process.downscale_img(downscaled32)
         downscaled8 = img_process.downscale_img(downscaled16)
         downscaled4 = img_process.downscale_img(downscaled8)
-
-        similar_img_list = []
         
-        lowest = 100
-        list_size = 0
+        print("\n---------------- 4x4 ---------------")
+        index_list4 = self.find_similar(downscaled4, self.data_set_4, None, .075, .0001)
+        if (len(index_list4)==0):
+            print("no match 4x4")
+            #some sort of error
+        print("\n---------------- 8x8 ---------------")
+        index_list8 = self.find_similar(downscaled8, self.data_set_8, index_list4, .07, 0) 
+        if (len(index_list8)==0):
+            print("no match 8x8")
+            return index_list4[random.randint(0, len(index_list4)-1)]
+        print("\n---------------- 16x16 ---------------")
+        index_list16 = self.find_similar(downscaled16, self.data_set_16, index_list8, .06, 0)
+        if (len(index_list16)==0):
+            print("no match 16x16")
+            return index_list8[random.randint(0, len(index_list8)-1)]
+        print("\n---------------- 32x32 ---------------")
+        index_list32 = self.find_similar(downscaled32, self.data_set_32, index_list16, .05, 0)
+        if (len(index_list32)==0):
+            print("no match 32x32")
+            return index_list16[random.randint(0, len(index_list16)-1)]
+        print("\n---------------- 64x64 ---------------")
+        index_list64 = self.find_similar(downscaled64, self.data_set_64, index_list32, .05, 0)
+        if (len(index_list64)==0):
+            print("no match 64x64")
+            return index_list32[random.randint(0, len(index_list32)-1)]
+        print("\n---------------- 126x128 ---------------")
+        index_list128 = self.find_similar(input_image, self.data_set, index_list64, .05, 0)
+        if (len(index_list128) == 0):
+            print("no match 128x128")
+            return index_list64[random.randint(0, len(index_list64)-1)]
+        return index_list128[random.randint(0, len(index_list128)-1)]
 
-        for i in range(self.data_set_4.shape[0]):
-            sum_of_dif = 0
-            for j in range(downscaled4.shape[0]):
-                dif_squared = (downscaled4[j] - self.data_set_4[i,0,j])**2
-                sum_of_dif += dif_squared
-            varience = sum_of_dif / downscaled4.shape[0]
-            if (varience < lowest):
-                lowest = varience
-            if(varience <= .075):
-                list_size += 1
-                similar_img_list.append(i)
-            print(varience)
-        print(lowest)
-        print(list_size)
+    def find_similar(self, input_img, data_set, img_index_list = None, variance_upper_bound = 1, varience_lower_bond = 0, drop_percent = .05):
+        lowest = 100
+        new_index_list = []
+
+        if(img_index_list == None):
+            for i in range(data_set.shape[0]):
+                sum_of_dif = 0
+                for j in range(input_img.shape[0]):
+                    dif_squared = (input_img[j] - data_set[i,0,j])**2
+                    sum_of_dif += dif_squared
+                varience = sum_of_dif / (input_img.shape[0] - 1) 
+                if(varience < variance_upper_bound and varience > varience_lower_bond):
+                    if (varience < lowest):
+                        lowest = varience
+                    new_index_list.append(i)
+            print(f"Lowest Variance in 4x4 similiarity is {lowest}")
+            print(f"List size after 4x4 run is {len(new_index_list)}")
+        else:
+            for i in range(len(img_index_list)):
+                sum_of_dif = 0 
+                for j in range(input_img.shape[0]):
+                    dif_squared = (input_img[j] - data_set[img_index_list[i],0,j])**2 
+                    sum_of_dif += dif_squared
+                varience = sum_of_dif / (input_img.shape[0] - 1) 
+                if(varience < variance_upper_bound and varience > varience_lower_bond):
+                    if (varience < lowest):
+                        lowest = varience
+                    new_index_list.append(i)
+            print(f"Lowest Variance in similiarity is {lowest}")
+            print(f"List size after run is {len(new_index_list)}")
+
+        new_index_list = self.drop_ran_from_list(drop_percent, new_index_list)
+        return new_index_list
+
+
+    def drop_ran_from_list(self, percentage, input_list):
+        list_size = len(input_list)
+        num_dropped = int(list_size * percentage) 
+        print(f"{num_dropped} items dropped from list")
+        for i in range(num_dropped):
+            drop_index = random.randint(0,list_size - 1 - i)
+            del input_list[drop_index]
+        print(f"New list size is {len(input_list)}")
+        return input_list
 
 
 
 img_process = img_processer("NPY_AllImageData16385.npy", "64x64_dataset.npy", "32x32_dataset.npy", "16x16_dataset.npy", "8x8_dataset.npy", "4x4_dataset.npy")
-img_process.input_data = img_process.get_single_image_data(12, 0)
-img_process.compare_img_with_dataset(img_process.input_data)
+img_process.input_data = img_process.get_single_image_data(0, 0)
+output_img = img_process.compare_img_with_dataset(img_process.input_data)
+print(f"output image index = {output_img}")
+img_process.canvas_np_img_to_png(img_process.data_set[output_img,0,:], "similar_img.png")
 #downscaled1 = img_process.downscale_img(img_process.input_data)
 #img_process.downscale_to_all_scales_and_save(img_process.data_set)
 
