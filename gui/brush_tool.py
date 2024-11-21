29affdc9e902d710c5cdd14c6f5484d99e25a3b8
Mitch Palczewski <135446256+mitchp819@users.noreply.github.com>
2024-11-21 18:14:45 -0500

    FloatBrushTool

--- gui/brush_tool.py
+++ gui/brush_tool.py
@@ -6,42 +6,58 @@ try:
 except ImportError:
     print("Error: windll not imported. Text may be blurred")
     pass
- 
+  
+
+import sys
+sys.path.append('C:/Users/mpalc/OneDrive/Desktop/_projects/next-brush-stroke-predictor_workspace/next_brush_stroke_predictor_app')
+from util import helper_functions as hlp_fun
 import window_header as wh
-import util.helper_functions as hlp_fun
+
 
 class BrushTool(tk.Tk):
-    def __init__(self, x = 10, y = 100, image_scalor = 6):
+    def __init__(self, x = 30, y = 200, image_scalor = 6):
         super().__init__()
-        self.geometry(f'120x500+{x}+{y}')
+        self.geometry(f'120x530+{x}+{y}')
         self.resizable(True, True)
         self.overrideredirect(True)
+        self.attributes
         self.img_sclr = image_scalor
 
         self.brush_size = tk.IntVar()
         self.brush_size.set(1)
         self.greyscale_value = tk.IntVar()
         self.greyscale_value.set(0)
-
+        
         wh.create_header_frame(self, True)
         self.create_sample_brush_frame()
         self.create_slider_frame()
         pass
 
+    def get_greyscale_value(self):
+        return self.greyscale_value
+    def get_brush_size(self):
+        return self.brush_size
+
     def create_sample_brush_frame(self):
         frame = tk.Frame(
             self,
-            height = 60,
-            width=60,
-            bg='white'
+            height = 80,
+            width= 80,
+            bg='lightblue',
+            border=4,
+            relief='raised'
         )
 
-        sample_brush = tk.Frame(
+        frame.pack_propagate(False)
+
+        self.sample_brush = tk.Canvas(
             frame,
-            bg='blue'
+            width = 30,
+            height = 30,
+            bg = 'black'
         )
-        sample_brush.pack(anchor='center')
-        frame.pack(padx=2,pady=2)
+        self.sample_brush.pack(expand=True)
+        frame.pack(padx=2,pady=5)
         pass
 
     def create_slider_frame(self):
@@ -49,8 +65,10 @@ class BrushTool(tk.Tk):
         slider_frame.columnconfigure(0, weight=1)
         slider_frame.columnconfigure(1, weight=1)
 
-        brush_size_label = tk.Label(slider_frame, text="Size")
-        brush_size_label.grid(column=0, row=0, pady=3)
+        self.greyscale_value_label = tk.Label(slider_frame, text="x")
+        self.greyscale_value_label.grid(column=1, row=0)
+        self.brush_size_label = tk.Label(slider_frame, text="x")
+        self.brush_size_label.grid(column=0, row=0)
 
         brush_size_slider = tk.Scale(
             slider_frame,
@@ -61,12 +79,10 @@ class BrushTool(tk.Tk):
             variable=self.brush_size,
             showvalue=False,
             width=30,
-            bg='lightblue'
+            bg='lightblue',
+            command = self.update_sample_brush
         )
         brush_size_slider.grid(column=0,row=1, padx=3,pady=3)
-
-        greyscale_value_label = tk.Label(slider_frame, text="Color")
-        greyscale_value_label.grid(column=1, row=0, pady=3)
 
         greyscale_value_slider = tk.Scale(
             slider_frame,
@@ -77,25 +93,32 @@ class BrushTool(tk.Tk):
             variable=self.greyscale_value,
             showvalue=False,
             width=30,
-            bg='lightblue'
+            bg='lightblue',
+            command = self.update_sample_brush
         )
         greyscale_value_slider.grid(column=1,row=1, padx=3, pady=3)
+        
+        greyscale_value_txt = tk.Label(slider_frame, text="Color")
+        greyscale_value_txt.grid(column=1, row=3)
+        brush_size_txt = tk.Label(slider_frame, text="Size")
+        brush_size_txt.grid(column=0, row=3)
+
         slider_frame.pack()
+        self.update_sample_brush(0)
         pass
 
-    def update_sample_brush(self):
+    def update_sample_brush(self,v):
         greyscale_hex = hlp_fun.greyscale_value_to_hex(self.greyscale_value.get())
         sample_width = self.brush_size.get() + self.img_sclr
-        sample_padding = ((self.max_brush_size + self.canvas_scalor) - sample_width)//2
         self.sample_brush.config(
             width = sample_width,
             height = sample_width,
-            bg=greyscale_hex
+            bg = greyscale_hex
         )
-        self.sample_brush.pack_configure(pady = sample_padding + 10)
+        self.greyscale_value_label.config(text=self.greyscale_value.get())
+        self.brush_size_label.config(text= self.brush_size.get())
         pass
 
 if __name__ == "__main__":
     brush_tool = BrushTool()
     brush_tool.mainloop()
-        
